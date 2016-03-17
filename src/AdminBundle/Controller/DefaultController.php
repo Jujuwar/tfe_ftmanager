66110a204a85f5c9fd814d8c28d6034c9139deb0
Chavée Julien <julien.chavee@gmail.com>
2016-03-17 16:07:17 +0100

    Début de la gestion des news

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -10,4 +10,13 @@ class DefaultController extends Controller
     {
         return $this->render('AdminBundle:Default:index.html.twig');
     }
+
+    public function newsAction()
+    {
+        $rep = $this->getDoctrine()->getRepository('MainBundle:News');
+
+        $array_news = $rep->findAll();
+
+        return $this->render('AdminBundle:News:index.html.twig', array( 'array_news' => $array_news ));
+    }
 }
