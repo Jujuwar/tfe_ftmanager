8ca83e0e5599922a458e1453c767e5d28253d8d2
Chavée Julien <julien.chavee@gmail.com>
2016-03-31 22:16:11 +0200

    - Gestion des news - Les changements de rôle sont correctement mis à jour durant la session

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -2,21 +2,137 @@
 
 namespace AdminBundle\Controller;
 
+use MainBundle\Entity\News;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        return $this->render( 'AdminBundle:Default:index.html.twig' );
     }
 
     public function newsAction()
     {
-        $rep = $this->getDoctrine()->getRepository('MainBundle:News');
+        $rep = $this->getDoctrine()->getRepository( 'MainBundle:News' );
 
         $array_news = $rep->findAll();
 
-        return $this->render('AdminBundle:News:index.html.twig', array( 'array_news' => $array_news ));
+        return $this->render( 'AdminBundle:News:index.html.twig', array( 'array_news' => $array_news ) );
+    }
+    
+    public function ajax_addAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            // TODO : Validation des données
+            $em = $this->getDoctrine()->getManager();
+            $user= $this->get( 'security.token_storage' )->getToken()->getUser();
+
+            $news = new News();
+            $news->setAuthor( $user );
+            $news->setTitle( $request->get('title') );
+            $news->setMessage( $request->get('message') );
+            $news->setPublishDate( new \DateTime() );
+            
+            $em->persist( $news );
+            $em->flush();
+
+            $response = new Response( json_encode( array( 'status' => 'ok', 'return' => $this->render( 'AdminBundle:News:newsRow.html.twig', array( 'news' => $news, 'loop' => array( 'index' => '-' ) ) )->getContent() ) ) );
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajax_deleteAction( Request $request ) {
+        // TODO : Meilleure gestion des erreurs (news n'existe pas, ...)
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $news = $em->getRepository( 'MainBundle:News' )->findOneById( $request->get( 'id' ) );
+
+                $em->remove( $news );
+                $em->flush();
+
+                $response = new Response( json_encode( array( 'status' => 'ok' ) ) );
+
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajax_getAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $news = $em->getRepository( 'MainBundle:News' )->findOneById( $request->get( 'id' ) );
+
+                $serializer = $this->get('serializer');
+                $news = $serializer->normalize($news);
+
+                $response = new Response( json_encode( array( 'status' => 'ok', 'news' => $news ) ) );
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajax_editAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $news = $em->getRepository( 'MainBundle:News' )->findOneById( $request->get( 'id' ) );
+
+                $news->setTitle( $request->get( 'title' ) );
+                $news->setMessage( $request->get( 'message' ) );
+
+                $em->flush();
+
+                $response = new Response( json_encode( array( 'status' => 'ok' ) ) );
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
     }
 }
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -4,12 +4,14 @@ namespace UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
+class User extends BaseUser implements EquatableInterface
 {
     /**
      * @ORM\Id
@@ -22,5 +24,22 @@ class User extends BaseUser
     {
         parent::__construct();
         // your own logic
+    }
+
+    public function isEqualTo(UserInterface $user)
+    {
+        if ($user instanceof User) {
+            // Check that the roles are the same, in any order
+            $isEqual = count($this->getRoles()) == count($user->getRoles());
+            if ($isEqual) {
+                foreach($this->getRoles() as $role) {
+                    $isEqual = $isEqual && in_array($role, $user->getRoles());
+                }
+            }
+            
+            return $isEqual;
+        }
+
+        return false;
     }
 }
