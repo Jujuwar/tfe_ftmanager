d34f8a670b37426a78d1a0b6c2858d47d8cd8a22
Chavée Julien <julien.chavee@gmail.com>
2016-04-11 09:55:59 +0200

    - Gestion des utilisateurs - Upgrade to symfony 2.8.4

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -2,11 +2,15 @@
 
 namespace AdminBundle\Controller;
 
+use AdminBundle\Form\EditUser;
 use MainBundle\Entity\News;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UserBundle\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -14,6 +18,119 @@ class DefaultController extends Controller
     {
         return $this->render( 'AdminBundle:Default:index.html.twig' );
     }
+
+    /* Gestion des utilisateurs */
+
+    public function usersAction() {
+        $rep = $this->getDoctrine()->getRepository( 'UserBundle:User' );
+
+        $array_users = $rep->findAll();
+
+        return $this->render( 'AdminBundle:User:index.html.twig', array( 'array_users' => $array_users ) );
+    }
+
+    public function usersEditAction( $id ) {
+        $userManager = $this->get( 'fos_user.user_manager' );
+
+        $user = $userManager->findUserBy( array( 'id' => $id ) );
+
+        $form = $this->createForm( EditUser::class, $user );
+
+        if( $form->isSubmitted() ) {
+            $userManager->updateUser( $user );
+        }
+
+        return $this->render( 'AdminBundle:User:edit.html.twig', array( 'user' => $user, 'form' => $form->createView() ) );
+    }
+
+    public function ajax_userPromoteAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $em->getRepository( 'UserBundle:User' )->findOneById( $request->get( 'id' ) );
+
+            if( $user->hasRole( 'ROLE_SUPER_ADMIN' ) && !$this->get( 'security.authorization_checker' )->isGranted( 'ROLE_SUPER_ADMIN' ) ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Vous n\'avez pas la permission pour cela' ) ) );
+                $response->headers->set( 'Content-Type', 'application/json') ;
+
+                return $response;
+            } else if( $user->hasRole( 'ROLE_ADMIN' ) && !$this->get( 'security.authorization_checker' )->isGranted( 'ROLE_SUPER_ADMIN' ) ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Vous n\'avez pas la permission pour cela' ) ) );
+                $response->headers->set( 'Content-Type', 'application/json') ;
+
+                return $response;
+            } else {
+                // TODO : Vérifier les différents groupes à supprimer
+                switch( $request->get( 'role' ) ) {
+                    case 'sup_admin':
+                        $user->addRole( 'ROLE_SUPER_ADMIN' );
+                        break;
+
+                    case 'admin':
+                        $user->addRole( 'ROLE_ADMIN' );
+                        break;
+
+                    case 'arbitre':
+                        $user->addRole( 'ROLE_ARBITRE' );
+                        break;
+
+                    case 'member':
+                        // TODO
+                        break;
+
+                }
+
+                $em->persist( $user );
+                $em->flush();
+
+                $response = new Response( json_encode( array( 'status' => 'ok', 'debug' => $user->hasRole( 'ROLE_ADMIN') ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json') ;
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajax_userDeleteAction( Request $request ) {
+        // TODO : Meilleure gestion des erreurs (utilisateur n'existe pas, ...)
+        if( $request->isXmlHttpRequest() ) {
+            if( $this->get( 'security.authorization_checker' )->isGranted( 'ROLE_SUPER_ADMIN' ) ) {
+                try {
+                    $em = $this->getDoctrine()->getManager();
+
+                    $user = $em->getRepository( 'UserBundle:User' )->findOneById( $request->get( 'id' ) );
+
+                    $em->remove( $user );
+                    $em->flush();
+
+                    $response = new Response( json_encode( array( 'status' => 'ok' ) ) );
+
+                }
+                catch( \Exception $e ) {
+                    $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
+                }
+            } else {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Vous n\'avez pas la permission de supprimer des utilisateurs' ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    /* Gestion des news */
 
     public function newsAction()
     {
@@ -24,7 +141,7 @@ class DefaultController extends Controller
         return $this->render( 'AdminBundle:News:index.html.twig', array( 'array_news' => $array_news ) );
     }
     
-    public function ajax_addAction( Request $request ) {
+    public function ajax_newsAddAction( Request $request ) {
         if( $request->isXmlHttpRequest() ) {
             // TODO : Validation des données
             $em = $this->getDoctrine()->getManager();
@@ -51,7 +168,7 @@ class DefaultController extends Controller
         return $response;
     }
 
-    public function ajax_deleteAction( Request $request ) {
+    public function ajax_newsDeleteAction( Request $request ) {
         // TODO : Meilleure gestion des erreurs (news n'existe pas, ...)
         if( $request->isXmlHttpRequest() ) {
             try {
@@ -80,7 +197,7 @@ class DefaultController extends Controller
         return $response;
     }
 
-    public function ajax_getAction( Request $request ) {
+    public function ajax_newsGetAction( Request $request ) {
         if( $request->isXmlHttpRequest() ) {
             try {
                 $em = $this->getDoctrine()->getManager();
@@ -107,7 +224,7 @@ class DefaultController extends Controller
         return $response;
     }
 
-    public function ajax_editAction( Request $request ) {
+    public function ajax_newsEditAction( Request $request ) {
         if( $request->isXmlHttpRequest() ) {
             try {
                 $em = $this->getDoctrine()->getManager();
---
+++ src/AdminBundle/Form/EditUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType ;
+
+class EditUser extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add( 'username', TextType::class, array( 'label' => "Nom d'utilisateur" ) )
+            ->add( 'email', EmailType::class )
+            /*->add( 'password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => array( 'label' => 'Mot de passe' ),
+                'second_options' => array( 'label' => 'Confirmer le mot de passe' ),
+            ) )*/
+            ->add( 'save', SubmitType::class, array( 'label' => 'Editer', 'attr' => array( 'class' => 'btn btn-primary' ) ) )
+            ->getForm();
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User',
+            'validation_groups' => false,
+        ));
+    }
+}
