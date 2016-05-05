49bae7de216a2747600e69683faa00b41ad014f1
Chavée Julien <julien.chavee@gmail.com>
2016-05-05 17:09:02 +0200

    - Lorsqu'une équipe supprime un joueur, celui-ci n'est pas totalement supprimé, son équipe est mise à null - Séparation du controller "Default" en plusieurs controller (index/user/default)

--- src/TeamBundle/Controller/PlayerController.php
+++ src/TeamBundle/Controller/PlayerController.php
@@ -3,61 +3,13 @@
 namespace TeamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use TeamBundle\Entity\Player;
-use TeamBundle\Form\TeamType;
-use TeamBundle\Entity\Team;
 
-class DefaultController extends Controller
+class PlayerController extends Controller
 {
-    public function indexAction()
-    {
-        $user = $this->getUser();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $team = $em->getRepository( 'TeamBundle:Team' )->findOneByManager( $user->getId() );
-
-        if($team) {
-            $players = $em->getRepository('TeamBundle:Player')->findBy(array('team' => $team->getId()));
-
-            return $this->render('TeamBundle:Default:index.html.twig', array('team' => $team, 'players' => $players));
-        } else // Si aucune équipe inscrite, on redirige sur la page pour en inscrire une
-            return $this->redirectToRoute( 'team_registration' );
-
-    }
-
-    public function registrationAction( Request $request ) {
-        $user = $this->getUser();
-
-        if( !empty( $user ) && empty( $this->getDoctrine()->getRepository( 'UserBundle:User' )->getTeam( $user->getId() ) ) ) {
-            $em = $this->getDoctrine()->getManager();
-
-            $team = new Team();
-
-            $team->setManager($user);
-            $team->setValid(false);
-
-            $form = $this->createForm(TeamType::class, $team);
-
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $team->setRegistrationDate(new \DateTime());
-
-                $em->persist($team);
-                $em->flush();
-
-                $this->redirectToRoute('team_homepage');
-            }
-
-            return $this->render('TeamBundle:Default:registration.html.twig', array('form' => $form->createView()));
-        } else
-            return $this->redirectToRoute( 'team_homepage' );
-    }
-    
-    public function ajaxPlayerAddAction( Request $request ) {
+    public function ajaxAddAction( Request $request ) {
         if( $request->isXmlHttpRequest() ) {
             // TODO : Meilleure validation des données
 
@@ -84,12 +36,11 @@ class DefaultController extends Controller
 
         $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
         $response->headers->set( 'Content-Type', 'application/json');
-        
+
         return $response;
     }
 
-    public function ajaxPlayerDeleteAction( Request $request ) {
-        // TODO : On supprime le joueur ou on lui enlève juste son équipe ?
+    public function ajaxDeleteAction( Request $request ) {
         if( $request->isXmlHttpRequest() ) {
             try {
                 $user= $this->get( 'security.token_storage' )->getToken()->getUser();
@@ -101,7 +52,7 @@ class DefaultController extends Controller
                 if( $player->getTeam()->getManager() == $user ) {
 
                     if ($player) {
-                        $em->remove($player);
+                        $player->setTeam(null);
                         $em->flush();
 
                         $response = new Response(json_encode(array('status' => 'ok')));
---
+++ src/TeamBundle/Controller/TeamController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace TeamBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use TeamBundle\Entity\Player;
+use TeamBundle\Form\TeamType;
+use TeamBundle\Entity\Team;
+
+class TeamController extends Controller
+{
+    public function indexAction()
+    {
+        $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $team = $em->getRepository( 'TeamBundle:Team' )->findOneByManager( $user->getId() );
+
+        if($team) {
+            $players = $em->getRepository('TeamBundle:Player')->findBy(array('team' => $team->getId()));
+
+            return $this->render('TeamBundle:Default:index.html.twig', array('team' => $team, 'players' => $players));
+        } else // Si aucune équipe inscrite, on redirige sur la page pour en inscrire une
+            return $this->redirectToRoute( 'team_registration' );
+
+    }
+
+    public function registrationAction( Request $request ) {
+        $user = $this->getUser();
+
+        if( !empty( $user ) && empty( $this->getDoctrine()->getRepository( 'UserBundle:User' )->getTeam( $user->getId() ) ) ) {
+            $em = $this->getDoctrine()->getManager();
+
+            $team = new Team();
+
+            $team->setManager($user);
+            $team->setValid(false);
+
+            $form = $this->createForm(TeamType::class, $team);
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $team->setRegistrationDate(new \DateTime());
+
+                $em->persist($team);
+                $em->flush();
+
+                $this->redirectToRoute('team_homepage');
+            }
+
+            return $this->render('TeamBundle:Default:registration.html.twig', array('form' => $form->createView()));
+        } else
+            return $this->redirectToRoute( 'team_homepage' );
+    }
+}
