895f2b41a99d452a6ce59b9ae83b52e96180aba0
Chavée Julien <julien.chavee@gmail.com>
2016-05-17 16:10:38 +0200

    - Début de la gestion des teams côté admin (Suppression de team ok)

---
+++ src/AdminBundle/Controller/TeamController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TeamController extends Controller
+{
+    public function indexAction()
+    {
+        $rep = $this->getDoctrine()->getRepository( 'TeamBundle:Team' );
+
+        $array_teams = $rep->findAll();
+
+        return $this->render( 'AdminBundle:Team:index.html.twig', array( 'array_teams' => $array_teams ) );
+    }
+
+    public function ajaxDeleteAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $team = $em->getRepository( 'TeamBundle:Team' )->findOneById( $request->get( 'id' ) );
+
+                if( $team ) {
+                    $em->remove( $team );
+                    $em->flush();
+
+                    $response = new Response( json_encode( array( 'status' => 'ok' ) ) );
+                } else
+                    $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'La team n\'existe pas' ) ) );
+
+
+            }
+            catch( \Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Impossible de supprimer une équipe ayant déjà participé à un tournoi ou possédant des joueurs', 'debug' => $e->getMessage() ) ) );
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Une erreur inconnue s\'est produite', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+    $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Accès non autorisé', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+}
---
+++ src/AdminBundle/Resources/public/js/team.js
@@ -0,0 +1,46 @@
+$('.table_teams_tbody').on('click', 'button[data-action="delete"]', function() {
+    var id = $(this).data('id');
+    var modal = $('.modal_alert_confirmation');
+
+    modal.find('.modal-body-more-info').text('La suppression de l\'équipe n\'est possible que si celle-ci n\'a participé à aucun tournoi et ne possède aucun joueur');
+    modal.find('.modal-confirmation-yes').data('id', id);
+    modal.modal('show');
+});
+
+$('.modal-confirmation-yes').on('click', function() {
+    var id = $(this).data('id');
+
+    $.ajax({
+        type: 'POST',
+        url: Routing.generate('admin_teams_ajax_delete'),
+        data: {
+            id: id
+        },
+        error: function (request, error) { // Info Debuggage si erreur
+            console.log("Erreur : responseText: " + request.responseText);
+        },
+        success: function (data) {
+            if(data.status == 'ok') {
+                $('tr[data-id="' + id + '"]').fadeOut(1500, "easeOutExpo", function() {
+                    $(this).remove();
+                    if($('.table_teams_tbody tr').length == 0) {
+                        $('.table_teams_tbody').append(
+                            $('<tr>').append(
+                                $('<td>').attr('colspan', '6').html('<i>Aucune team inscrite</i>')
+                            )
+                        );
+                    }
+                });
+                $('.modal_alert_success').modal('show');
+                setTimeout(function(){
+                    $(".modal_alert_success").modal('hide');
+                }, 1700);
+            } else {
+                $('.modal-body-more-info').html(data.message);
+                $('.modal_alert_error').modal('show');
+
+                console.log(data.debug);
+            }
+        }
+    });
+});
