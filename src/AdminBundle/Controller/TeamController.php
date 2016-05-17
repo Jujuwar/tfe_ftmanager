5321ab782807c3d0c6c26f4a4d0a55fba3b31fb9
Chavée Julien <julien.chavee@gmail.com>
2016-05-17 17:01:09 +0200

    - (Admin) Gestion de la validation des équipes

--- src/AdminBundle/Controller/TeamController.php
+++ src/AdminBundle/Controller/TeamController.php
@@ -30,9 +30,7 @@ class TeamController extends Controller
 
                     $response = new Response( json_encode( array( 'status' => 'ok' ) ) );
                 } else
-                    $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'La team n\'existe pas' ) ) );
-
-
+                    $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'La team n\'existe pas', 'debug' => 'La team n\'existe pas' ) ) );
             }
             catch( \Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException $e ) {
                 $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Impossible de supprimer une équipe ayant déjà participé à un tournoi ou possédant des joueurs', 'debug' => $e->getMessage() ) ) );
@@ -47,6 +45,36 @@ class TeamController extends Controller
         }
 
     $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Accès non autorisé', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajaxValidateAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $team = $em->getRepository( 'TeamBundle:Team' )->findOneById( $request->get( 'id' ) );
+
+                if( $team ) {
+                    $team->setValid( !$team->getValid() );
+                    $em->flush();
+
+                    $response = new Response( json_encode( array( 'status' => 'ok', 'return' => $this->render( 'AdminBundle:Team:teamRow.html.twig', array( 'team' => $team ) )->getContent() ) ) );
+                } else
+                    $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'La team n\'existe pas', 'debug' => 'La team n\'existe pas' ) ) );
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Une erreur inconnue s\'est produite', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'message' => 'Accès non autorisé', 'debug' => 'Bad request' ) ) );
         $response->headers->set( 'Content-Type', 'application/json') ;
 
         return $response;
--- src/AdminBundle/Resources/public/js/team.js
+++ src/AdminBundle/Resources/public/js/team.js
@@ -44,3 +44,37 @@ $('.modal-confirmation-yes').on('click', function() {
         }
     });
 });
+
+$('.table_teams_tbody').on('click', 'button[data-action="validate"]', function() {
+    var id = $(this).data('id');
+
+    console.log('test');
+
+    $.ajax({
+        type: 'POST',
+        url: Routing.generate('admin_teams_ajax_validate'),
+        data: {
+            id: id
+        },
+        error: function (request, error) { // Info Debuggage si erreur
+            console.log("Erreur : responseText: " + request.responseText);
+        },
+        success: function (data) {
+            if(data.status == 'ok') {
+                var line = $('tr[data-id="' + id + '"]');
+                line.replaceWith(data.return);
+                line.effect("highlight", {color: '#c9c9c9'}, 5000);
+
+                $('.modal_alert_success').modal('show');
+                setTimeout(function(){
+                    $(".modal_alert_success").modal('hide');
+                }, 1700);
+            } else {
+                $('.modal-body-more-info').html(data.message);
+                $('.modal_alert_error').modal('show');
+
+                console.log(data.debug);
+            }
+        }
+    });
+});
