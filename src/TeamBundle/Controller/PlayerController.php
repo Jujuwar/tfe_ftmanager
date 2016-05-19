398f90e4eda9d549a9377e355801a11a582b289c
Chavée Julien <julien.chavee@gmail.com>
2016-05-19 14:52:53 +0200

    - Possibilité d'éditer un joueur

--- src/TeamBundle/Controller/PlayerController.php
+++ src/TeamBundle/Controller/PlayerController.php
@@ -5,6 +5,8 @@ namespace TeamBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use TeamBundle\Entity\Player;
 
 class PlayerController extends Controller
@@ -60,6 +62,37 @@ class PlayerController extends Controller
                 } else
                     $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Vous n\'avez pas la permission de supprimer ce joueur' ) ) );
 
+            }
+            catch( \Exception $e ) {
+                $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
+            }
+
+            $response->headers->set( 'Content-Type', 'application/json' );
+
+            return $response;
+        }
+
+        $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => 'Bad request' ) ) );
+        $response->headers->set( 'Content-Type', 'application/json') ;
+
+        return $response;
+    }
+
+    public function ajaxGetAction( Request $request ) {
+        if( $request->isXmlHttpRequest() ) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $player = $em->getRepository( 'TeamBundle:Player' )->findOneById( $request->get( 'id' ) );
+
+                $normalizer  = new ObjectNormalizer();;
+                $normalizer->setCircularReferenceHandler(function ($object) {
+                    return $object->getId();
+                });
+                $serializer = new Serializer( array( $normalizer ) );
+                $player = $serializer->normalize( $player );
+
+                $response = new Response( json_encode( array( 'status' => 'ok', 'player' => $player ) ) );
             }
             catch( \Exception $e ) {
                 $response = new Response( json_encode( array( 'status' => 'ko', 'debug' => $e->getMessage() ) ) );
--- src/TeamBundle/Resources/public/js/player.js
+++ src/TeamBundle/Resources/public/js/player.js
@@ -20,9 +20,11 @@ $('.addPlayer').on('click', function() {
             type: 'POST',
             url: Routing.generate('admin_news_ajax_edit'),
             data: {
-                id: id,
-                title: title,
-                message: message
+                team: team,
+                number: number,
+                name: name,
+                surname: surname,
+                birthday: birthday
             },
             error: function (request, error) { // Info Debuggage si erreur
                 console.log("Erreur : responseText: " + request.responseText);
@@ -32,8 +34,8 @@ $('.addPlayer').on('click', function() {
                     modal.modal('hide');
 
                     var line = $('tr[data-id="' + id + '"]');
+                    line.replaceWith(data.return);
                     line.effect("highlight", {color: '#c9c9c9'}, 5000);
-                    line.children("td:nth-child(2)").html(title);
 
                     $('.modal_alert_success').modal('show');
                     setTimeout(function () {
@@ -121,4 +123,53 @@ $('.table_players_tbody').on('click', 'button[data-action="delete"]', function()
 
     modal.find('.modal-confirmation-yes').data('id', id);
     modal.modal('show');
+});
+
+$('.table_players_tbody').on('click', 'button[data-action="edit"]', function() {
+    var id = $(this).data('id');
+    var modal = $('#addPlayer');
+
+    $.ajax({
+        type: 'POST',
+        url: Routing.generate('team_player_ajax_get'),
+        data: {
+            id: id
+        },
+        error: function (request, error) { // Info Debuggage si erreur
+            console.log("Erreur : responseText: " + request.responseText);
+        },
+        success: function (data) {
+            if(data.status == 'ok') {
+                modal.find('#addPlayer_Number').val(data.player.number);
+                modal.find('#addPlayer_Surname').val(data.player.surname);
+                modal.find('#addPlayer_Name').val(data.player.name);
+                modal.find('#addPlayer_Birthday').val(moment.unix(data.player.birthday.timestamp).format('DD/MM/YYYY'));
+
+                console.log(data.player.birthday);
+
+                modal.find('.modal-title').html("Modification d'un joueur");
+                modal.find('.fa-plus').removeClass('fa-plus').addClass('fa-pencil');
+
+                modal.find('.addPlayer').data('id', id).data('edit', '1');
+                modal.modal('show');
+            } else {
+                $('.modal-body-more-info').html(data.debug);
+                $('.modal_alert_error').modal('show');
+            }
+        }
+    });
+});
+
+$('#addPlayer').on('hide.bs.modal', function() {
+    var button = $(this).find('.addNews');
+
+    $(this).find('#addPlayer_Number').val('');
+    $(this).find('#addPlayer_Surname').val('');
+    $(this).find('#addPlayer_Name').val('');
+    $(this).find('#addPlayer_Birthday').val('');
+    $(this).find('.modal-title').html("Ajouter un joueur");
+
+    button.find('.fa').removeClass('fa-pencil fa-pulse fa-spinner').addClass('fa-plus');
+    button.removeAttr('disabled');
+    button.removeData('edit');
 });
